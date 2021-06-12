f521360ff43798db65fe21bb5c3ccc320c203a1d
Michael Ryan <michael_ryan_2000@yahoo.com>
2021-06-12 02:31:45 -0400

    Running end to end deployment test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a deployment <code>test</code>.
         </p>
         <a
           className="App-link"
